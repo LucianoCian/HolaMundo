d593c8e5c6b6c4c812a7000486c2833b980c7fa2
LucianoCian <cian_luciano@hotmail.com>
2023-03-22 09:03:36 -0300

    Ejercicio 2

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-# Desde la consola de python almacena la cadena “Hola mundo” en una variable y muestrala.
+# Modifica la variable del anterior ejercicio en la consola de python y después muestrala por consola para ver la modificación de la variable.
 Saludo = 'Hola, mi nombre es Luciano Cian'
 print(Saludo)
