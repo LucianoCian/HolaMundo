d3cc194e76bfd5550c92a05ac1392f3d95dc8e78
LucianoCian <cian_luciano@hotmail.com>
2023-03-22 18:08:36 -0300

    Ejercicio 3

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-# Modifica la variable del anterior ejercicio en la consola de python y después muestrala por consola para ver la modificación de la variable.
-Saludo = 'Hola, mi nombre es Luciano Cian'
-print(Saludo)
+# Escribe un programa en la consola de python que pida al usuario su peso (en kg) y estatura (en metros), calcule el índice de masa corporal y lo almacene en una variable, e imprima por pantalla la frase Tu índice de masa corporal es donde es el índice de masa corporal calculado redondeado con dos decimales.
+def imc(altura, peso):
+    'Calcular el indice de masa corporal: '
+    return (peso/altura**2)
+
+peso = float(input('Ingresar tu peso en kg: '))
+altura = float(input('Ingresar tu altura en metros: '))
+indice= imc(altura, peso)
+print('Tu indice de masa corporal es :  {:.2f}'.format(indice))
