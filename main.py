66c7084e8e52607ed7cfbbfd3b01687892df8e76
LucianoCian <cian_luciano@hotmail.com>
2023-03-23 16:55:10 -0300

    Ejercicio 4c

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.
+# Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso
 
 for i in range(100, 0, -1):
     print(i)
