d94870d9f183d7d15f0538affd04db56c6b0cbbe
LucianoCian <cian_luciano@hotmail.com>
2023-03-23 16:49:48 -0300

    Ejercicio 4

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-# Escribe un programa en la consola de python que pida al usuario su peso (en kg) y estatura (en metros), calcule el índice de masa corporal y lo almacene en una variable, e imprima por pantalla la frase Tu índice de masa corporal es donde es el índice de masa corporal calculado redondeado con dos decimales.
-def imc(altura, peso):
-    'Calcular el indice de masa corporal: '
-    return (peso/altura**2)
+# Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.
 
-peso = float(input('Ingresar tu peso en kg: '))
-altura = float(input('Ingresar tu altura en metros: '))
-indice= imc(altura, peso)
-print('Tu indice de masa corporal es :  {:.2f}'.format(indice))
+for i in range(100, 0, -1):
+    print(i)
